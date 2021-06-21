49944a61dc89cc5fe33916ac8fd320c808ebf4d3
Javier Diaz <hey@coderdiaz.me>
2021-06-21 17:09:24 -0500

    fix: minor fixes

--- components/Onboarding.js
+++ components/Onboarding.js
@@ -1,5 +1,5 @@
 import cn from "classnames"
-import { useState } from "react"
+import React, { useState } from "react"
 import { ArrowRightIcon } from "@heroicons/react/outline"
 
 function Onboarding({
@@ -12,18 +12,18 @@ function Onboarding({
   const handleChange = (index) => setActiveSlot(index)
 
   // Validation for prevent errors in rendering
-  if (children.length === 0) return null
-  if ((children.length - 1) < initialPage) throw new Error(`The initialPage prop value ${initialPage} is out of range`)
+  if (!children || React.Children.count(children) === 0) return null
+  if ((React.Children.count(children) - 1) < initialPage) throw new Error(`The initialPage prop value ${initialPage} is out of range`)
 
   if (!skipHandler) skipHandler = () => {} // Added base function isn't provided
   if (!completeHandler) completeHandler = () => {} // Added base function isn't provided
   
   return <div className="flex">
-    {children[activeSlot]}
+    {React.Children.count(children) > 1 ? children[activeSlot] : children }
 
     <div className="absolute w-full bottom-4 z-50">
       <ul className="flex space-x-2 justify-center items-center py-6">
-        {children.map((_, i) => 
+        {React.Children.map(children, (_, i) => 
           <li
             className={
               cn("rounded-full bg-white bg-opacity-75 w-1.5 h-1.5 outline-none focus:outline-none", {
@@ -34,18 +34,20 @@ function Onboarding({
             aria-label={`Page ${i}`} />)}
       </ul>
       <div className="flex justify-between">
-        { activeSlot !== children.length - 1 
+        { activeSlot !== React.Children.count(children) - 1
           && <button
                 onClick={skipHandler}
+                name="Skip"
                 className="px-4 py-3 text-white text-opacity-75 focus:outline-none">Skip</button>}
-        { activeSlot < children.length - 1
+        { activeSlot < React.Children.count(children) - 1
           && <button
                 onClick={() => handleChange(activeSlot + 1)}
+                name="Next"
                 className="px-4 py-3 font-bold text-white focus:outline-none flex items-center space-x-2">
                   <span className="inline-block">Next</span>
                   <ArrowRightIcon className="w-4 h-4" />
                 </button> }
-        { activeSlot === children.length - 1
+        { React.Children.count(children) === 0 || activeSlot === React.Children.count(children) - 1
           && <button
                 onClick={completeHandler}
                 className="mx-4 px-4 py-3 font-bold bg-white w-full rounded-full focus:outline-none">Getting Started</button> }
